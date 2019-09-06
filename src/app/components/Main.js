83beb7368c72e65a6ceb5817ea75deffc55af274
NKV-88 <sysadm86@gmail.com>
2019-09-06 17:07:58 +0500

    1 часть урока.

---
+++ src/app/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Главная страница</h1>
+                <p>Добро пожаловать на наш сайт!</p>
+            </div>
+        );
+    }
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require ("path");
 
 module.exports = {
-    entry: "./src/app.js",
+    entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname,"dist")
@@ -20,5 +20,9 @@ module.exports = {
             }
 
         ]
+    },
+    devServer: {
+        historyApiFallback: true,         
+        contentBase: path.resolve(__dirname, "dist"),
     }
 };
